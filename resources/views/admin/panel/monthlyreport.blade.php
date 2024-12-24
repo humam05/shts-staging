21eb8f89b5936ca13a0c290484c2411e0611b858
Adil Fathi <54827419+adilfathi@users.noreply.github.com>
2024-12-24 15:15:40 +0700

    Fix Minor Bugs

--- resources/views/admin/panel/monthlyreport.blade.php
+++ resources/views/admin/panel/monthlyreport.blade.php
@@ -38,7 +38,7 @@
                     <label for="transaksi_akhir" class="block text-sm font-medium text-gray-700 mb-2">Tanggal Transaksi
                         Terakhir</label>
                     <input type="month" name="transaksi_akhir" id="transaksi_akhir"
-                        class="border border-gray-300 rounded-lg w-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500">
+                        class="border border-gray-300 rounded-lg w-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" value="{{ request('transaksi_akhir') }}">
                 </div>
 
                 {{-- Status Lunas --}}
@@ -62,7 +62,7 @@
                 </div>
 
                 
-                <{{-- Tombol Filter --}} <div class="flex space-x-2 items-end">
+                <div class="flex space-x-2 items-end">
                     <button type="submit" name="filter" value="filter"
                         class="bg-indigo-600 text-white py-2 px-4 rounded-md shadow hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-300">
                         Terapkan Filter
